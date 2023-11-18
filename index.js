49b56c553069a92f26f19c6cf486163de2f2da9d
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-11-18 13:17:31 +0600

    final push

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let menu_icon=document.getElementById('menu_icon')
+let body=document.body
 menu_icon.addEventListener('click', ()=>{
     document.getElementById("sidebar").classList.add("active");
-    document.getElement('body').classList.add("overlay")
+    body.classList.add('menu-active')
 })
 let cross_icon=document.getElementById('cross_icon')
 cross_icon.addEventListener('click', ()=>{
     document.getElementById("sidebar").classList.remove("active");
-    document.getElement('body').classList.remove("overlay")
+    body.classList.remove('menu-active')
 })
