cc7033a89c8455927db4d8a0d5c2f2a1a3e5473b
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-11-20 15:56:57 +0600

    just template added

--- README.md
+++
@@ -1,3 +0,0 @@
-# FrontEndFusion
-
-This is a project for the competition front-end fusion hosted by IEEE CS on 18th of November 2023
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let menu_icon=document.getElementById('menu_icon')
-let body=document.body
-menu_icon.addEventListener('click', ()=>{
+fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('nav-component').innerHTML = data;
+    });
+    
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-component').innerHTML = data;
+    });
+
+
+let menu_icon = document.getElementById('menu_icon')
+menu_icon.addEventListener('click', () => {
     document.getElementById("sidebar").classList.add("active");
-    body.classList.add('menu-active')
+    document.body.classList.add("menu-active")
 })
-let cross_icon=document.getElementById('cross_icon')
-cross_icon.addEventListener('click', ()=>{
+let cross_icon = document.getElementById('cross_icon')
+cross_icon.addEventListener('click', () => {
     document.getElementById("sidebar").classList.remove("active");
-    body.classList.remove('menu-active')
+    document.body.classList.remove("menu-active")
 })
