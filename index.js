edf37d7f660104f73d4b9b5b1269fc65f99aaa59
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-11-21 10:22:54 +0600

    just template added

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ fetch('nav.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('nav-component').innerHTML = data;
+        window.addEventListener('load', function () {
+            let menu_icon = document.getElementById('menu_icon')
+            let cross_icon = document.getElementById('cross_icon')
+            let nav = document.getElementById('sidebar')
+            menu_icon.addEventListener('click', () => {
+                nav.classList.add("active");
+                document.body.classList.add("menu-active")
+            })
+            function close_menu() {
+                nav.classList.remove("active");
+                document.body.classList.remove("menu-active")
+            }
+            document.body.addEventListener('click', (e) => {
+                if (nav.classList.contains('active') && e.target != menu_icon && (e.target != nav || e.target == cross_icon)) {
+                    console.log(true)
+                    close_menu()
+                }
+            })
+
+        })
     });
-    
+
 fetch('footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-component').innerHTML = data;
     });
 
-
-let menu_icon = document.getElementById('menu_icon')
-menu_icon.addEventListener('click', () => {
-    document.getElementById("sidebar").classList.add("active");
-    document.body.classList.add("menu-active")
-})
-let cross_icon = document.getElementById('cross_icon')
-cross_icon.addEventListener('click', () => {
-    document.getElementById("sidebar").classList.remove("active");
-    document.body.classList.remove("menu-active")
-})
